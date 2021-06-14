b7c26d7dad0f4f92c0ab495b1a80dd2d7089af8a
Miguel Díaz <mdiazl@vectoritcgroup.com>
2021-06-14 15:00:32 +0200

    Primer commit Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <img alt="Vue logo" src="./assets/Pirate.png">
+    <ListaDivisas titulo="Lista divisas!!" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ListaDivisas from "./components/ListaDivisas";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ListaDivisas
   }
 }
 </script>
---
+++ src/components/ListaDivisas.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="lista-divisas">
+        <h1>Lista divisas</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ListaDivisas',
+        props: {
+            titulo: String
+        }
+    }
+
+</script>
+
+<style>
+
+</style>
+
+
+  
+
+
